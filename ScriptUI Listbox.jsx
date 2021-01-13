c6d8e00f79b1819634d2f55e621ba4a101ffb07b
Nate Lovell <Natelovell99@gmail.com>
2021-01-13 02:07:04 -0700

    Create ScriptUI Listbox.jsx

---
+++ ScriptUI Listbox.jsx
@@ -0,0 +1,30 @@
+﻿// ScriptUI Listboxes
+var counter = 0;
+
+var window = new Window("palette", "Listbox", undefined);
+window.orientation = "column";
+var listBox = window.add("listbox", undefined, []);
+listBox.selection = 0;
+listBox.size = [70, 60];
+
+var buttonGroup = window.add("group", undefined, "buttonGroup");
+buttonGroup.orientation = "row";
+var addButton = buttonGroup.add("button", undefined, "+");
+addButton.size = [25, 25];
+var minusButton = buttonGroup.add("button", undefined, "-");
+minusButton.size = [25, 25];
+
+addButton.onClick = function () {
+        counter++;
+        listBox.add("item", "Item_"+counter.toString());
+    }
+
+minusButton.onClick = function() {
+        if(listBox.selection != null) {
+        counter--;
+        listBox.remove(listBox.selection);
+            }
+    }
+
+window.center();
+window.show();
